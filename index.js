2cea301d1e8b1984515e15fb12800b814afbc9f0
elpatron <aidanseantucker@gmail.com>
2023-07-05 07:45:02 -0400

    changles URL portion of API fetch request to troubleshoot not getting a expected response when calling it.

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ const searchBtn = document.getElementById('search-btn');
 const playerStats = document.getElementById('player-stats');
 
 // Event listeners
-searchBtn.addEventListener('click', searchPlayer);
+searchBtn.addEventListener('click', searchPlayer); 
+//mkae a submit
 
 // Function to search for NBA player stats
 function searchPlayer() {
   const playerName = searchInput.value.trim();
+  console.log(playerName);
 
   // Call the API with playerName and process the response
   fetchPlayerStats(playerName);
@@ -22,10 +24,11 @@ function searchPlayer() {
 
 // Function to fetch player stats from the API
 async function fetchPlayerStats(playerName) {
-  const url = `https://api-nba-v1.p.rapidapi.com/players?search=${playerName}&per_page=1`;
+  const url = `https://api-nba-v1.p.rapidapi.com/players?name=${playerName}`;
   const options = {
     method: 'GET',
     headers: {
+      "Content-Type": "application/json",
       'X-RapidAPI-Key': '4948651216msh7c93eb95da5ba5fp10b6b3jsn2cffd22451d6',
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
@@ -34,6 +37,7 @@ async function fetchPlayerStats(playerName) {
   try {
     const response = await fetch(url, options);
     const data = await response.json();
+    console.log(data);
 
     if (data.data && data.data.length > 0) {
       const player = data.data[0];
@@ -48,6 +52,7 @@ async function fetchPlayerStats(playerName) {
 
 // Function to display player stats on the page
 function displayPlayerStats(player) {
+  console.log(player);
   playerStats.innerHTML = `
     <h3>Player: ${player.first_name} ${player.last_name}</h3>
     <p>Team: ${player.team.full_name}</p>
