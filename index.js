7aeaec87deab453a404713774f41d20ae87795bc
elpatron <aidanseantucker@gmail.com>
2023-07-07 15:58:56 -0400

    finally figured out how to display the data fetched from the api

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchButton = document.getElementById('search-btn');
   const teamStats = document.getElementById('team-stats');
 
-  // Event Listener 1: Button click
   searchButton.addEventListener('click', () => {
     const teamName = searchInput.value;
     searchTeamStats(teamName);
@@ -17,13 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   teamStats.addEventListener('mouseover', () => {
-    teamStats.style.backgroundColor = 'white';
+    teamStats.style.backgroundColor = 'green';
   });
 
   async function searchTeamStats(teamName) {
     const apiKey = '4948651216msh7c93eb95da5ba5fp10b6b3jsn2cffd22451d6';
-  
-    const url = `https://api-basketball.p.rapidapi.com/statistics?season=2019-2020&league=12&team=${teamName}`;
+
+    const url = `https://api-basketball.p.rapidapi.com/teams?league=12&season=2019-2020&search=${teamName}`;
     const options = {
       method: 'GET',
       headers: {
@@ -31,46 +30,37 @@ document.addEventListener('DOMContentLoaded', () => {
         'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
       }
     };
-  
+
     try {
       const response = await fetch(url, options);
       if (response.ok) {
         const result = await response.json();
         console.log(result);
-        teamStats.textContent = result;
-        displayTeamStats(result);
+
+        if (result.response.length > 0) {
+          const firstResponse = result.response[0];
+          displayTeamStats(firstResponse);
+        } else {
+          teamStats.textContent = 'Team not found.';
+        }
       } else {
-        console.log("Error with format");
+        console.log('Error with format');
       }
     } catch (error) {
       console.error(error);
-      teamStats.textContent = 'Error retrieving team statistics.';
     }
   }
 
-  function displayTeamStats(result) {
-    const teamStats = document.getElementById('team-stats');
-    
-    teamStats.innerHTML = '';
-  
-    if (result.errors) {
-      teamStats.textContent = 'Error retrieving team statistics.';
-      return;
-    }
-  
-    const teamName = result.response.team.name;
-    const games = result.response.games;
-    const points = result.response.points;
-  
-    // Create new content
-    const content = `
-      <ul>
-        <li>Team: ${teamName}</li>
-        <li>Games: ${games}</li>
-        <li>Points: ${points}</li>
-      </ul>
+  function displayTeamStats(team) {
+    const { country, logo, name } = team;
+
+    const htmlContent = `
+      <h2>${name}</h2>
+      <p>Country: ${country.name}</p>
+      <img src="${logo}" alt="${name} logo">
     `;
-  
-    teamStats.innerHTML = content;
+
+    const responseContainer = document.getElementById('response-container');
+    responseContainer.innerHTML = htmlContent;
   }
-})
+});
